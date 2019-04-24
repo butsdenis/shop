6d59346c761ab9fb8bc7b5c92c172e974d8d51a9
v1try <butsdenis@gmail.com>
2019-04-24 15:43:54 +0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: './modules/auth/auth.module#AuthModule'
+  },
+  {
+    path: 'admin',
+    loadChildren: './modules/admin/admin-panel.module#AdminPanelModule'
   }
 ];
 
